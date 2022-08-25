c227e60923aae5466b3e12b692027905de5087a9
w8385 <w8385@naver.com>
2022-08-26 04:02:24 +0900

    input, arithmetic_operations

---
+++ 2.arithmetic_operations.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %d\n", x / y);
+    printf("x % y = %d\n", x % y);
+
+    return 0;
+}
+
+/*
+ * print arithmetic operations of two input values
+ */
---
+++ 2.arithmetic_operations.py
@@ -0,0 +1,13 @@
+(x, y) = input().split(' ')
+x = int(x)
+y = int(y)
+
+print("x + y = " + str(x + y))
+print("x - y = " + str(x - y))
+print("x * y = " + str(x * y))
+print("x / y = " + str(x // y))
+print("x % y = " + str(x % y))
+
+'''
+print arithmetic operations of two input values
+'''
