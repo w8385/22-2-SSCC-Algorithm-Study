60c789b9896f7ce829c126c4b8726a4cd5a0ea0d
w8385 <w8385@naver.com>
2022-09-14 18:29:32 +0900

    출력

--- cpp/0.printf.cpp
+++ cpp/0.printf.cpp
@@ -1,10 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello SSCC!");
+    // 기본 출력
+    printf("Hello SSCC!\n");
+
+    // 정수 자료형 및 출력
+    int n = 1983;
+    printf("%d\n", n);
+
+    // 실수 자료형 및 출력
+    float pi = 3.14;
+    printf("%f\n", pi);
+
+    // 문자 자료형 및 출력
+    char c = 'c';
+    printf("%c\n", c);
+
+    // 특수 문자 출력
+    printf("%% ");
+    printf("\\ ");
+    printf("\' ");
+    printf("\" ");
+
     return 0;
 }
-
 /*
  * Hello SSCC!
+ * 1983
+ * 3.14
+ * c
  */
