093a3d015d848af5aa3b1c55c37f60912137f1a8
w8385 <w8385@naver.com>
2022-09-14 18:46:25 +0900

    입력

--- cpp/1.scanf.cpp
+++ cpp/1.scanf.cpp
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    scanf("%d", &n);
+    // 문자 입출력
+    char c;
+    printf("input char :");
+    scanf("%c", &c);
+    printf("%c\n", c);
 
-    printf("%d", n);
+    // 문자열 입출력
+    char s[10];
+    printf("input string :");
+    scanf("%s", s);
+    printf("%s\n", s);
+
+    // 정수 입출력
+    int n;
+    printf("input int :");
+    scanf("%d", &n);
+    printf("%d\n", n);
+
+    // 실수 입출력
+    float f;
+    printf("input float :");
+    scanf("%f", &f);
+    printf("%f\n", f);
 
     return 0;
 }
-
-/*
- * print input
- */
