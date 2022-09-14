23a76b1fdb0e65bed9dde133e39aa82a3decf66f
w8385 <w8385@naver.com>
2022-09-14 19:34:30 +0900

    출력

--- py/0.print.py
+++ py/0.print.py
@@ -1,5 +1,27 @@
-print("Hello SSCC!")
+# 기본 출력
+print("Hello SSCC!\n")
+
+# 정수 출력 및 자료형
+n = 1983
+print(n)
+# print(str(type(n)) + "\n")
+
+# 실수 출력 및 자료형
+f = 3.14
+print(f)
+# print(str(type(f)) + "\n")
+
+# 문자 출력 및 자료형
+c = 'c'
+print(c)
+# print(str(type(c)) + "\n")
 
 '''
 Hello SSCC!
+
+1983
+
+3.14
+
+c
 '''
